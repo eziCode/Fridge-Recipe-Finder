3dac5a2f48536e6fd8f89dc2936b01872c9345bc
eziCode <ezraakresh@gmail.com>
2024-12-02 10:55:07 -0600

    Initial Commit

---
+++ FridgeRecipeFinder/FridgeRecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeRecipeFinderApp.swift
+//  FridgeRecipeFinder
+//
+//  Created by Ezra Akresh on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FridgeRecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridgeRecipeFinderTests/FridgeRecipeFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeRecipeFinderTests.swift
+//  FridgeRecipeFinderTests
+//
+//  Created by Ezra Akresh on 12/2/24.
+//
+
+import Testing
+@testable import FridgeRecipeFinder
+
+struct FridgeRecipeFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
